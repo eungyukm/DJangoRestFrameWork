34bb5ba51fa7ddaedf4dd7dee3011f9bd369ffdf
eungyukm <eungyukm@gmail.com>
2022-08-01 19:33:02 +0900

    upload

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def __str__(self) -> str:
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,118 @@
+from ast import Return
+from urllib import response
+from django.shortcuts import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import status, generics, mixins
+from rest_framework.decorators import api_view, APIView
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import serializers
+from django.shortcuts import render
 
-posts = [
-    {
-        "id": 1,
-        "title": "floor",
-        "content": "This is Floor"
-    },
-    {
-        "id": 2,
-        "title": "Door",
-        "content": "This is Door"
-    },
-    {
-        "id": 3,
-        "title": "Wall",
-        "content": "This is Wall"
-    }
-]
+# posts = [
+#     {
+#         "id": 1,
+#         "title": "floor",
+#         "content": "This is Floor"
+#     },
+#     {
+#         "id": 2,
+#         "title": "Door",
+#         "content": "This is Door"
+#     },
+#     {
+#         "id": 3,
+#         "title": "Wall",
+#         "content": "This is Wall"
+#     }
+# ]
+
+
+class PostListCreateView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request: Request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request: Request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostRetrieveUpdateDeleteView(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request: Request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request: Request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request: Request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+# class PostListCreateView(APIView):
+#     serializer_class = PostSerializer
+
+#     def get(self, request: Request, *args, **kwargs):
+#         posts = Post.objects.all()
+
+#         serializer = PostSerializer(instance=posts, many=True)
+
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+#     def post(self, reqeust: Request, *args, **kwargs):
+#         data = reqeust.data
+
+#         serializers = self.serializer_class(data=data)
+
+#         if serializers.is_valid():
+#             serializers.save()
+
+#             response = {
+#                 "message": "Post Created",
+#                 "data": serializers.data
+#             }
+#             return Response(data=response, status=status.HTTP_201_CREATED)
+
+
+# class PostRetrieveUpdateDeleteView(APIView):
+#     serializer_class = PostSerializer
+
+#     def get(self, request: Request, post_id: int):
+#         post = get_object_or_404(Post, pk=post_id)
+
+#         serializers = self.serializer_class(instance=post)
+
+#         return Response(data=serializers.data, status=status.HTTP_200_OK)
+
+#     def put(self, request: Request, post_id: int):
+#         post = get_object_or_404(Post, pk=post_id)
+
+#         data = request.data
+
+#         serializer = self.serializer_class(instance=post, data=data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+
+#             response = {
+#                 "message": "Post Updated",
+#                 "data": serializer.data
+#             }
+#             return Response(data=response, status=status.HTTP_200_OK)
+
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request: Request, post_id: int):
+#         post = get_object_or_404(Post, pk=post_id)
+
+#         post.delete()
+
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(http_method_names=["GET", "POST"])
@@ -34,16 +127,75 @@ def homepage(request: Request):
     return Response(data=response, status=status.HTTP_200_OK)
 
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def list_posts(request: Request):
-    return Response(data=posts, status=status.HTTP_200_OK)
+    posts = Post.objects.all()
+    if request.method == "GET":
+        serializers = PostSerializer(instance=posts, many=True)
+        response = {
+            "message": "GET All",
+            "data": serializers.data
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+
+    elif request.method == "POST":
+        data = request.data
+
+        serializers = PostSerializer(data=data)
+
+        if serializers.is_valid():
+            serializers.save()
+
+            response = {
+                "message": "Post Created",
+                "data": serializers.data
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response(data={"error": "post not found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(http_method_names=["GET"])
 def post_detail(request: Request, post_id: int):
-    post = posts[post_id]
+    if request.method == "GET":
+        posts = Post.objects.filter(id=post_id)
+        serializers = PostSerializer(instance=posts, many=True)
+        response = {
+            "message": "posts",
+            "data": serializers.data
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+    else:
+        return Response(data={"error": "post not found"}, status=status.HTTP_404_NOT_FOUND)
 
-    if post:
-        return Response(data=post, status=status.HTTP_200_OK)
 
-    return Response(data={"error": "post not found"}, status=status.HTTP_404_NOT_FOUND)
+@api_view(http_method_names=["PUT"])
+def update_post(request: Request, post_id: int):
+    post = get_object_or_404(Post, pk=post_id)
+
+    data = request.data
+
+    serializers = PostSerializer(instance=post, data=data)
+
+    if serializers.is_valid():
+        serializers.save()
+
+        response = {
+            "message": "Post Updated Successfully",
+            "data": serializers.data,
+        }
+
+        return Response(data=response, status=status.HTTP_200_OK)
+
+    return Response(data=serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(http_method_names=["DELETE"])
+def delete_post(request: Request, post_id: int):
+    post = get_object_or_404(Post, pk=post_id)
+
+    post.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
